016586253b95c2fbd4ec6e3f5838bd2568ff019e
Eric Peterson <i.am@eric.pe>
2019-11-18 19:28:45 -0800

    Make up for vimeo iframe height sadness. Don't mangle blockquote styles.

--- gatsby-config.js
+++ gatsby-config.js
@@ -78,7 +78,11 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
+        whitelist: [
+          '.post-content-body blockquote',
+          '.post-content-body blockquote p', 
+          '.post-content-body blockquote p:last-child',
+        ],
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
