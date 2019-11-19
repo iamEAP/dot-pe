779616b4d6d97ade08f03c1eecddf468b049db96
Eric Peterson <i.am@eric.pe>
2019-11-18 21:01:21 -0800

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c2e497641d42c6767185.js"
+    "url": "webpack-runtime-79e34c5bcbdfb35908ab.js"
   },
   {
     "url": "styles.e30e414e9cdf03d6db9e.css"
@@ -45,7 +45,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c731122752e182e8a5b63137ad1b23cb"
+    "revision": "c79cb350359660d9bef8763be995f3af"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
