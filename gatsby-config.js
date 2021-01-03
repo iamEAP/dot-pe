be5fba88520eea32a549e729f9c41bb43e45e23d
Eric Peterson <i.am@eric.pe>
2021-01-03 14:10:45 +0100

    Fix autoprefixer config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
           require("postcss-easy-import")(),
           require("postcss-custom-properties")({ preserve: false }),
           require("postcss-color-function")(),
-          require("autoprefixer")({ browsers: ["last 2 versions"] }),
+          require("autoprefixer")(),
         ],
       },
     },
@@ -78,9 +78,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        whitelistPatternsChildren: [
-          '/post-content-body/'
-        ],
+        whitelistPatternsChildren: ["/post-content-body/"],
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
