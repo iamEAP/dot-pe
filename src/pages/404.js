08acfa20d6dddc561c5f518a3b95e15f94cfe4fc
Eric Peterson <i.am@eric.pe>
2021-01-07 18:56:36 +0100

    Make the 404 page language-aware.

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,37 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { withTranslation } from "react-i18next"
 
-import Layout from "../components/layout"
+import { default as EnLayout } from "../layouts/en"
+import { default as SvLayout } from "../layouts/sv"
 import SEO from "../components/seo"
 
-class NotFoundPage extends React.Component {
+class PreNotFoundPage extends React.Component {
   render() {
-    const { data } = this.props
+    const { t, i18n } = this.props
+    const { data, location } = this.props
     const siteTitle = data.site.siteMetadata.title
+    let langLocale = "en-US"
+    let LocalLayout = EnLayout
+
+    if (location.pathname.indexOf("/sv") === 0) {
+      i18n.changeLanguage("sv")
+      LocalLayout = SvLayout
+      langLocale = "sv-SE"
+    }
 
     return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="404: Not Found" />
-        <h1>Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </Layout>
+      <LocalLayout location={location} title={siteTitle}>
+        <SEO title={`404: ${t("Not Found")}`} lang={langLocale} />
+        <div className="not-found">
+          <h1>{t("Not Found")}</h1>
+          <p>{t("You just hit a page that doesn't exist")}.</p>
+        </div>
+      </LocalLayout>
     )
   }
 }
+const NotFoundPage = withTranslation()(PreNotFoundPage)
 
 export default NotFoundPage
 
