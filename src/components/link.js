2b54f890d0db46338117126da4fb5870cc1588ba
Eric Peterson <i.am@eric.pe>
2021-01-07 18:48:39 +0100

    Introducing wrapper around Gatsby Link to help handle localized links

---
+++ src/components/link.js
@@ -0,0 +1,34 @@
+/**
+ * Link component that wraps Gatsby's for smarter l10n logic.
+ */
+
+import React, { useContext } from "react"
+import { Link as GatsbyLink } from "gatsby"
+import { LanguageContext } from "../contexts/language"
+import { getSlugForPost } from "../utils/i18n-urls"
+
+function Link(props) {
+  const [{ langKey }] = useContext(LanguageContext)
+  const propsCopy = Object.assign({}, props)
+  let localizedUrl = getSlugForPost(langKey, props.to)
+
+  if (props.forceLang && props.forceLang !== langKey) {
+    const matches = localizedUrl.match(/^(\/[a-z]{2})\//)
+    // Non-English to English
+    if (matches && matches[1]) {
+      localizedUrl = localizedUrl.replace(matches[1], "")
+    }
+    // English to non-English
+    else {
+      localizedUrl = `/${props.forceLang}${localizedUrl}`
+    }
+  }
+
+  // Apply localized URL and cleanup non-standard props.
+  propsCopy.to = localizedUrl
+  delete propsCopy.forceLang
+
+  return <GatsbyLink {...propsCopy} />
+}
+
+export default Link
