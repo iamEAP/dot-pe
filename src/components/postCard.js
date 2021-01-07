e7d835d1e92e70b067959ac79c24b33c4df569e7
Eric Peterson <i.am@eric.pe>
2021-01-07 18:54:43 +0100

    Update PostCard component to use new wrapped link Component.

--- src/components/postCard.js
+++ src/components/postCard.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link } from "gatsby"
+import Link from "./link"
 
 export default props => (
   <article
@@ -8,9 +8,7 @@ export default props => (
     } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
     style={
       props.node.frontmatter.thumbnail && {
-        backgroundImage: `url(${
-          props.node.frontmatter.thumbnail.childImageSharp.fluid.src
-        })`,
+        backgroundImage: `url(${props.node.frontmatter.thumbnail.childImageSharp.fluid.src})`,
       }
     }
   >
