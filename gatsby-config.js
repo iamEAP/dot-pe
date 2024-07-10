936397fb08c9dd18b48d8a63cd42bf5b5184bddc
Eric Peterson <i.am@eric.pe>
2024-07-10 13:03:40 +0200

    fix: postcss dep vulns

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,6 @@ module.exports = {
         postCssPlugins: [
           require("postcss-easy-import")(),
           require("postcss-custom-properties")({ preserve: false }),
-          require("postcss-color-function")(),
           require("autoprefixer")(),
         ],
       },
